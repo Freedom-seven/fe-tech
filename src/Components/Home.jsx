600231cec883caeec974a6222038fa8a84159271
FREEDOM <freewhynane62@gmail.com>
2021-07-23 14:10:40 +0000

    add navbar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const home = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">FE Tech</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            className="justify-content-end"
+            id="responsive-navbar-nav"
+          >
+            <Nav>
+              <Nav.Link href="#deets">Add Employee</Nav.Link>
+              <Nav.Link href="#pricing">View Employee List</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 
 export default home;
