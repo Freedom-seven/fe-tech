62613afc97c56ea8a150775cfb4a6d37f304ddc0
FREEDOM <freewhynane62@gmail.com>
2021-07-23 13:03:19 +0000

    import useState

--- src/Components/EmployeeData.jsx
+++ src/Components/EmployeeData.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Col, Modal, Button} from "react-bootstrap";
+import EditEmployeeDataForm from './EditEmployeeDataForm'
 
 const EmployeeData = ({dataList}) => {
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  
+
     return (
+        <>
+        <Col md="4">
         <div>
             <p>First Name: <strong>{dataList.firstName}</strong> </p>
             <p>Last Name: <strong>{dataList.lastName}</strong> </p>
@@ -10,7 +21,22 @@ const EmployeeData = ({dataList}) => {
             <p>Home Address: <strong>{dataList.homeAddress}</strong> </p>
             <p>Job Role: <strong>{dataList.jobRole}</strong> </p>
             <p>Current Salary: <strong>{dataList.currentSalary}</strong> </p>
+
+            <Button variant="success" onClick={handleShow} >Edit</Button>
+            <Button variant="danger" onClick={handleClose} >Delete</Button>
         </div>
+        </Col>
+
+        <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Data</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <EditEmployeeDataForm />
+        </Modal.Body>
+      </Modal>
+        
+        </>
     );
 }
 
