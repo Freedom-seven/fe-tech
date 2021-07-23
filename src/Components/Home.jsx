872d0817f64306f52c00e7c571913450da0a856a
FREEDOM <freewhynane62@gmail.com>
2021-07-23 15:25:08 +0000

    add span

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "./home.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 const home = () => {
   return (
-    <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        className="navbar-home"
+      >
         <Container>
           <Navbar.Brand href="#home">FE Tech</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -19,7 +26,46 @@ const home = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </div>
+      <div className="home">
+        <h1>FE TECH</h1>
+        <h3>OUR MISSION</h3>
+        <p>
+          We provide lifecycle power solutions to enhance the business of our
+          <br />
+          customers, whilst creating better technologies that benefit both the
+          <br />
+          customer and the environment.
+        </p>
+        <h3>OUR VISION</h3>
+        <p>We will be the most valued business partner of all our customers.</p>
+        <h3>OUR VALUES</h3>
+        <p>
+          <span>* </span>
+          <i>Conduct business with integrity & fairness</i>
+          <br />
+
+          <span>* </span>
+          <i>Focus on our customer's needs</i>
+          <br />
+
+          <span>* </span>
+          <i>Continuously train our employees & improve our processes</i>
+          <br />
+
+          <span>* </span>
+          <i>Providequality products & services</i>
+          <br />
+
+          <span>* </span>
+          <i>Reward employees on merit & promote teamwork</i>
+          <br />
+
+          <span>* </span>
+          <i>Maintain safe & healthy working environment</i>
+          <br />
+        </p>
+      </div>
+    </>
   );
 };
 
