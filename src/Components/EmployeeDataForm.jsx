105019b91c4045febb4404ac7f6bac1fd1508cce
FREEDOM <freewhynane62@gmail.com>
2021-07-23 09:52:16 +0000

    remove row and col

--- src/Components/EmployeeDataForm.jsx
+++ src/Components/EmployeeDataForm.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 
@@ -20,7 +18,7 @@ class EmployeeDataForm extends Component {
         }
     }
 
-    
+
     render() {
         return (
             <Form onSubmit={this.handleSubmit}>
