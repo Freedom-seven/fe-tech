ac34d5627e4a086ca0f20b38b82a463933b4a9ba
FREEDOM <freewhynane62@gmail.com>
2021-07-23 08:45:25 +0000

    add components

---
+++ src/Components/EmployeeData.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmployeeData = () => {
+    return (
+        <div>
+            <h1>Employee Data</h1>
+        </div>
+    );
+}
+
+export default EmployeeData;
---
+++ src/Components/EmployeeDataList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import EmployeeData from './EmployeeData';
+
+
+const EmployeeDataList = () => {
+    return (
+        <div>
+            <h1>Employee Data List</h1>
+            <EmployeeData />
+        </div>
+    );
+}
+
+export default EmployeeDataList;
