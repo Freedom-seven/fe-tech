3f44f340db9873e85537d75cb5368a1dce33245a
FREEDOM <freewhynane62@gmail.com>
2021-07-23 08:30:28 +0000

    add object to array

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: [
+        {
+          firstName: "Freedom",
+          lastName: "Nanewortor",
+          email: "freedom@email.com",
+          phoneNumber: "020000002",
+          homeAddress: "GX3000-04",
+          jobRole: "Software Developer",
+          currentSalary: "$10,000"
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div>
